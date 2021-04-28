9f6b1aebaf028f5b2c857712e973a76cbefbf5d2
Santhosh <58876990+santhoshpragada@users.noreply.github.com>
2021-04-28 21:24:14 +0530

    added mpg321 command

--- text_speech.py
+++ text_speech.py
@@ -5,3 +5,5 @@ b=gtts.gTTS(a,lang=language,slow=False)
 c=input("enter audio file name to save converted speech to audio")
 b.save(c+".mp3")
 print("pls find the file in the executed folder with given name")
+#os.system("mpg321 c.mp3")
+#The above os.system function can be used if you have mpg321 tool in windows
